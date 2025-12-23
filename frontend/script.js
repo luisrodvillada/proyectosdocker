102c3a9a6e81b618545c8086317113f3aefeb398
luisrodvillada <luisfernando198912@gmail.com>
2025-12-23 12:17:56 +0100

    docker: clean up stack, env config and healthchecks

--- frontend/script.js
+++
@@ -1,8 +0,0 @@
-  fetch("/api/hello")
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById("mensaje").innerText = data.message;
-  })
-  .catch(error => {
-    console.error("Error al consultar backend:", error);
-  });
